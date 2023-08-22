9aac2f212ff2ec2859ce18f88a9a79f8953fd84d
micaelatorretta <micaelavtorretta@gmail.com>
2023-08-22 12:25:09 -0300

    Post de UserController

--- MusicApp/Controllers/UsersController.cs
+++ MusicApp/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using MusicApp.Models.DTOs;
 using MusicApp.Models.Entities;
 using MusicApp.Models.Enums;
 using MusicApp.Repositories.Interfaces;
+using System.Net;
 using System.Text.RegularExpressions;
 
 namespace MusicApp.Controllers
@@ -88,7 +89,82 @@ namespace MusicApp.Controllers
             }
         }
 
-       
+        [HttpPost]
+        public IActionResult Post([FromBody] NewUserDTO newUserDTO)
+        {
+            try
+            {
+                // Validación de nombre y apellido
+                if (!IsNameValid(newUserDTO.FirstName) || !IsNameValid(newUserDTO.LastName))
+                {
+                    return StatusCode(400, "datos inválidos");
+                }
+                // Validación de contraseña
+                if (!IsPasswordValid(newUserDTO.Password))
+                {
+                    return StatusCode(400, "datos inválidos");
+                }
+                // Validación de dirección de correo electrónico
+                if (!IsValidEmail(newUserDTO.Email))
+                {
+                    return StatusCode(400, "Email inválido.");
+                }
+
+                // Validación de datos obligatorios
+                if (String.IsNullOrEmpty(newUserDTO.Email) || String.IsNullOrEmpty(newUserDTO.Password) || String.IsNullOrEmpty(newUserDTO.FirstName) || String.IsNullOrEmpty(newUserDTO.LastName))
+                    return StatusCode(403, "datos inválidos");
+
+                // Buscar si el correo electrónico ya está en uso
+                User user = _userRepository.FindByEmail(newUserDTO.Email);
+                if (user is not null)
+                {
+                    return StatusCode(403, "Email está en uso");
+                }
+
+                //Mapeamos el DTO que llega del front a Usuario para guardarlo en el repo
+                var newUser = _mapper.Map<User>(newUserDTO);
+                _userRepository.Save(newUser);
+
+                // Mapeamos el User a un UserDTO para devolver una respuesta de éxito
+                var userDTO = _mapper.Map<UserDTO>(newUser);
+
+                return Created("", userDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        private static bool IsNameValid(string name)
+        {
+            return Regex.IsMatch(name, @"^[a-zA-Z\s]+$") && name.Length >= 3;
+        }
+
+        private static bool IsPasswordValid(string password)
+        {
+            return Regex.IsMatch(password, @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$");
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            // Verificación básica del formato de correo electrónico usando una expresión regular
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                return false;
+            }
+
+            try
+            {
+                var mailAddress = new System.Net.Mail.MailAddress(email);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
     }
 }
