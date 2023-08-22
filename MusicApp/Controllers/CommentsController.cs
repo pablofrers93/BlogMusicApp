b62cf6c7beb11567ab0cbb24727278b6dda30f9a
Tomtechno <69020112+Tomtechno@users.noreply.github.com>
2023-08-22 12:26:31 -0800

    Comments Controller Test

---
+++ MusicApp/Controllers/CommentsController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApp.Repositories.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace MusicApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private ICommentRepository _commentRepository;
+        private readonly IMapper _mapper;
+
+        public CommentsController(ICommentRepository commentRepository, IMapper mapper)
+        {
+            _commentRepository = commentRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Client client)
+        {
+            try
+            {
+                //validaciones
+                Client user = _clientRepository.FindByEmail(client.Email);
+                if (user != null)
+                {
+                    return StatusCode(403, "El email está en uso");
+                }
+                Client newClient = new Client
+                {
+                    Email = client.Email,
+                    Password = client.Password,
+                    FirstName = client.FirstName,
+                    LastName = client.LastName,
+                };
+                _commentRepository.Save(newClient);
+                return Created("Creado con exito", newClient);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
