12f1ecd9417244038d5bda1a46eaec4dd41fdf97
micaelatorretta <micaelavtorretta@gmail.com>
2023-08-28 11:15:45 -0300

    JWT

--- MusicApp/Controllers/PostController.cs
+++ MusicApp/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MusicApp.Models.DTOs;
@@ -12,6 +13,7 @@ namespace MusicApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PostController : ControllerBase
     {
         private readonly IPostRepository _postRepository;
---
+++ MusicApp/Services/ITokenServices.cs
@@ -0,0 +1,7 @@
+﻿namespace MusicApp.Services
+{
+    public interface ITokenServices
+    {
+        string GenerateToken(string userEmail);
+    }
+}
