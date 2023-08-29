79cce04a878f931e98914dbb4abfb1e70521edf0
catalina-to24 <catalinatempraotero@gmail.com>
2023-08-29 09:49:13 -0300

    cambios en el dto y commentsController

--- MusicApp/Controllers/CommentsController.cs
+++ MusicApp/Controllers/CommentsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using MusicApp.Models.DTOs;
 using MusicApp.Models.Entities;
 using MusicApp.Repositories.Interfaces;
+using System.Security.Claims;
 
 namespace MusicApp.Controllers
 {
@@ -77,7 +78,7 @@ namespace MusicApp.Controllers
             try
             {
                 //validaciones
-                string email = User.FindFirst("User") != null ? User.FindFirst("User").Value : string.Empty;
+                string email = User.FindFirst(ClaimTypes.Email) != null ? User.FindFirst(ClaimTypes.Email).Value : string.Empty;
                 if (email == string.Empty)
                 {
                     return Unauthorized();
--- MusicApp/Models/DTOs/PostDTO.cs
+++ MusicApp/Models/DTOs/PostDTO.cs
@@ -4,8 +4,8 @@ namespace MusicApp.Models.DTOs
 {
     public class PostDTO
     {
-        public long Id { get; set; }
-        public DateTime CreationDate { get; set; }
+        //public long Id { get; set; }
+        //public DateTime CreationDate { get; set; }
         public string Title { get; set; }
         public IFormFile Image { get; set; }
         public string Text { get; set; }
